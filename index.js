2fbddbde17f5b5940b71a208c3c9f4ca75ef6617
fubits <ilja_s@protonmail.com>
2022-08-15 23:31:18 +0200

    bun init

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // http.js
 export default {
   port: 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
+  async fetch(request) {
+    const joke = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data.joke);
+
+    return new Response(joke);
   },
 };
